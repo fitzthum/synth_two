6653464fc4d0711579f3295a202e6c78c26a618b
Tobin Fitzthum <tobinf@protonmail.com>
2023-03-26 02:00:06 -0400

    Wrestle with CSS round 1
    Looks bad
    
    Signed-off-by: Tobin Fitzthum <tobinf@protonmail.com>

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -15,7 +15,7 @@ impl Model for Data {}
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::new(|| (1200, 1000))
+    ViziaState::new(|| (700, 700))
 }
 
 pub(crate) fn create(
@@ -25,6 +25,8 @@ pub(crate) fn create(
     create_vizia_editor(editor_state, ViziaTheming::Custom, move |cx, _| {
         assets::register_noto_sans_light(cx);
         assets::register_noto_sans_thin(cx);
+
+        cx.add_theme(include_str!("theme.css"));
 
         Data {
             params: params.clone(),
@@ -54,32 +56,46 @@ fn top(cx: &mut Context) {
 
 fn top_left(cx: &mut Context) {
     VStack::new(cx, |cx| {
-        Label::new(cx, "Gain");
-        ParamSlider::new(cx, Data::params, |params| &params.gain);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Gain").class("label").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.gain);
+        }).class("row");
 
         // Maybe move this to the middle at some point
-        Label::new(cx, "Oscillator Balance");
-        ParamSlider::new(cx, Data::params, |params| &params.oscillator_balance);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Oscillator Balance").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.oscillator_balance);
+        }).class("row");
  
-    });
+    })
+    .class("quarter");
 }
 
 fn top_right(cx: &mut Context) {
     VStack::new(cx, |cx| {
         // ADSR
-        Label::new(cx, "Attack");
-        ParamSlider::new(cx, Data::params, |params| &params.attack);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Attack").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.attack);
+        }).class("row");
 
-        Label::new(cx, "Decay");
-        ParamSlider::new(cx, Data::params, |params| &params.decay);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Decay").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.decay);
+        }).class("row");
 
-        Label::new(cx, "Sustain");
-        ParamSlider::new(cx, Data::params, |params| &params.sustain);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Sustain").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.sustain);
+        }).class("row");
 
-        Label::new(cx, "Release");
-        ParamSlider::new(cx, Data::params, |params| &params.release);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Release").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.release);
+        }).class("row");
 
-    });
+    })
+    .class("quarter");
 }
 
 fn oscillators(cx: &mut Context) {
@@ -93,48 +109,76 @@ fn oscillators(cx: &mut Context) {
 // in theory this should be generic but idk how
 fn oscillator1(cx: &mut Context) {
     VStack::new(cx, |cx| {
-        Label::new(cx, "Wave Index");
-        ParamSlider::new(cx, Data::params, |params| &params.wave_index_1);
+        Label::new(cx, "Oscillator 1").class("osc-title");
 
-        Label::new(cx, "Wave Warp");
-        ParamSlider::new(cx, Data::params, |params| &params.wave_warp_1);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Wave Index").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.wave_index_1);
+        }).class("row");
 
-        Label::new(cx, "Warp Attack");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_attack_1);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Wave Warp").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.wave_warp_1);
+        }).class("row");
 
-        Label::new(cx, "Warp Decay");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_decay_1);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Attack").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_attack_1);
+        }).class("row");
 
-        Label::new(cx, "Warp Sustain");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_sustain_1);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Decay").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_decay_1);
+        }).class("row");
 
-        Label::new(cx, "Warp Release");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_release_1);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Sustain").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_sustain_1);
+        }).class("row");
 
-    });
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Release").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_release_1);
+        }).class("row");
+
+    })
+    .class("quarter");
 }
 
 fn oscillator2(cx: &mut Context) {
     VStack::new(cx, |cx| {
-        Label::new(cx, "Wave Index");
-        ParamSlider::new(cx, Data::params, |params| &params.wave_index_2);
+        Label::new(cx, "Oscillator 2").class("osc-title");
 
-        Label::new(cx, "Wave Warp");
-        ParamSlider::new(cx, Data::params, |params| &params.wave_warp_2);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Wave Index").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.wave_index_2);
+        }).class("row");
 
-        Label::new(cx, "Warp Attack");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_attack_2);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Wave Warp").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.wave_warp_2);
+        }).class("row");
 
-        Label::new(cx, "Warp Decay");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_decay_2);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Attack").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_attack_2);
+        }).class("row");
 
-        Label::new(cx, "Warp Sustain");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_sustain_2);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Decay").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_decay_2);
+        }).class("row");
 
-        Label::new(cx, "Warp Release");
-        ParamSlider::new(cx, Data::params, |params| &params.warp_release_2);
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Sustain").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_sustain_2);
+        }).class("row");
 
-    });
+        HStack::new(cx, |cx| {
+            Label::new(cx, "Warp Release").class("label");
+            ParamSlider::new(cx, Data::params, |params| &params.warp_release_2);
+        }).class("row");
+
+    })
+    .class("quarter");
 }
-
-
