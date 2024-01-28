e9d8261423add42f204e2a358ab47ce0c50eecdd
Tobin Fitzthum <tobinf@protonmail.com>
2024-01-28 17:58:33 -0500

    Add saving by name
    It doesn't work...
    
    Not sure why the Textbox is not editable :'(
    
    Signed-off-by: Tobin Fitzthum <tobinf@protonmail.com>

--- src/editor/presets.rs
+++ src/editor/presets.rs
@@ -28,16 +28,11 @@ use std::collections::HashMap;
 const PRESETS_PATH: &str = "/tmp/presets.json";
 
 #[derive(Serialize, Deserialize)]
-struct Presets {
+pub struct Presets {
     presets: HashMap<String, PluginState>,
 }
 
 impl Presets {
-    fn new() -> Self {
-        Self {
-            presets: HashMap::new()
-        }
-    }
 
     // load tries to load from fs but falls back to the builtin
     fn load() -> Self {
@@ -45,12 +40,14 @@ impl Presets {
             true => std::fs::read(PRESETS_PATH).unwrap(),
             false => include_str!("../../presets.json").into(),
         };
-        serde_json::from_slice(&presets_string).unwrap()
+        Self {
+            presets: serde_json::from_slice(&presets_string).unwrap(),
+        }
     }
 
     // for now this will allow you to overwrite an existing preset
-    fn add(&mut self, name: String, state: PluginState) -> Result<()> {
-        self.presets.insert(name, state);
+    fn add(&mut self, name: &String, state: PluginState) -> Result<()> {
+        self.presets.insert(name.to_string(), state);
 
         // propogate the changes back to the fs
         self.save();
@@ -71,14 +68,17 @@ impl Presets {
     }
 }
 
+#[derive(Lens)]
 pub struct PresetMenu {
     gui_context: Arc<dyn GuiContext>,
     presets: Presets,
+    new_preset_name: String,
 }
 
 enum PresetMenuEvent {
     SavePreset,
     LoadPreset,
+    UpdateNewPresetName(String)
 }
 
 impl PresetMenu {
@@ -89,8 +89,8 @@ impl PresetMenu {
     {
         Self {
             gui_context: gcx,
-            // TODO: replace with load() once we have some base presets to load
             presets: Presets::load(),
+            new_preset_name: "".to_string(),
         }
             .build(cx, |cx| {
                 HStack::new(cx, |cx| {
@@ -99,6 +99,13 @@ impl PresetMenu {
 						|ex| ex.emit(PresetMenuEvent::SavePreset),
 						|cx| Label::new(cx, "Save Preset")
 					);
+                    Textbox::new(cx, PresetMenu::new_preset_name)
+                        .on_edit(|cx, text| cx.emit(PresetMenuEvent::UpdateNewPresetName(text)))
+                        .width(Pixels(200.0))
+                        .on_build(|cx| {
+                            cx.emit(TextEvent::StartEdit);
+                            cx.emit(TextEvent::SelectAll);
+                        });
 					Button::new(
 						cx,
 						|ex| ex.emit(PresetMenuEvent::LoadPreset),
@@ -112,15 +119,18 @@ impl PresetMenu {
 
 impl View for PresetMenu {
     fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
-        event.map(|preset_event, _meta| match preset_event{
+        event.map(|preset_event, _| match preset_event {
             PresetMenuEvent::SavePreset => {
-                self.presets.add("first".to_string(), self.gui_context.get_state()).unwrap();
+                self.presets.add(&self.new_preset_name, self.gui_context.get_state()).unwrap();
 
             }
             PresetMenuEvent::LoadPreset => {
                 let json_state = std::fs::read(PRESETS_PATH).unwrap();
                 self.gui_context.set_state(serde_json::from_slice(&json_state).unwrap());
 
+            }
+            PresetMenuEvent::UpdateNewPresetName(name) => {
+                self.new_preset_name = name.to_string();
             }
         });
     }
