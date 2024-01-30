6daf2d8bb63fc9ae5560820b4cbbc40b9facd2a7
Tobin Fitzthum <tobinf@protonmail.com>
2024-01-29 19:53:49 -0500

    Add preset loading.
    Complete with a really sketchy dropdown menu.
    
    Signed-off-by: Tobin Fitzthum <tobinf@protonmail.com>

--- src/editor/presets.rs
+++ src/editor/presets.rs
@@ -27,7 +27,7 @@ use std::collections::HashMap;
 // TODO: make this OS independent
 const PRESETS_PATH: &str = "/tmp/presets.json";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Presets {
     presets: HashMap<String, PluginState>,
 }
@@ -72,13 +72,16 @@ impl Presets {
 pub struct PresetMenu {
     gui_context: Arc<dyn GuiContext>,
     presets: Presets,
+    preset_names: Vec<String>,
     new_preset_name: String,
+	selected_preset: String,
 }
 
 enum PresetMenuEvent {
     SavePreset,
     LoadPreset,
-    UpdateNewPresetName(String)
+    UpdateNewPresetName(String),
+	UpdatePresetSelection(String),
 }
 
 impl PresetMenu {
@@ -87,18 +90,38 @@ impl PresetMenu {
         gcx: Arc<dyn GuiContext>,
     ) -> Handle<Self>
     {
+        let presets = Presets::load();
+        let preset_names = Vec::from_iter(presets.presets.clone().into_keys());
+
         Self {
             gui_context: gcx,
-            presets: Presets::load(),
+            presets: presets.clone(),
+            preset_names: preset_names.clone(), 
             new_preset_name: "".to_string(),
-        }
-            .build(cx, |cx| {
+			selected_preset: preset_names[0].clone(),
+        }.build(cx, |cx| {
                 HStack::new(cx, |cx| {
+                    // some kind of dropdown thing here
+                    ScrollView::new(cx, 0.0, 0.0, false, true, |cx| {
+                        VStack::new(cx, |cx| {
+                            for preset_name in preset_names {
+                                // TODO: check if this is the one that we
+                                // have selected and change the class
+                                Label::new(cx, &preset_name)
+                                    .on_press(move |cx| cx.emit(PresetMenuEvent::UpdatePresetSelection(preset_name.clone())));
+                            }
+
+                        });
+                    })
+                    .width(Pixels(100.0));
+
 					Button::new(
 						cx,
-						|ex| ex.emit(PresetMenuEvent::SavePreset),
-						|cx| Label::new(cx, "Save Preset")
+						|ex| ex.emit(PresetMenuEvent::LoadPreset),
+						|cx| Label::new(cx, "Load Preset")
 					);
+
+
                     Textbox::new(cx, PresetMenu::new_preset_name)
                         .on_edit(|cx, text| cx.emit(PresetMenuEvent::UpdateNewPresetName(text)))
                         .width(Pixels(200.0))
@@ -108,11 +131,12 @@ impl PresetMenu {
                         });
 					Button::new(
 						cx,
-						|ex| ex.emit(PresetMenuEvent::LoadPreset),
-						|cx| Label::new(cx, "Load Preset")
+						|ex| ex.emit(PresetMenuEvent::SavePreset),
+						|cx| Label::new(cx, "Save Preset")
 					);
 
-                });
+                })
+                .col_between(Pixels(20.0));
             })
     }
 }
@@ -125,12 +149,15 @@ impl View for PresetMenu {
 
             }
             PresetMenuEvent::LoadPreset => {
-                let json_state = std::fs::read(PRESETS_PATH).unwrap();
-                self.gui_context.set_state(serde_json::from_slice(&json_state).unwrap());
+                let state = self.presets.get(self.selected_preset.clone());
+                self.gui_context.set_state(state);
 
             }
             PresetMenuEvent::UpdateNewPresetName(name) => {
                 self.new_preset_name = name.to_string();
+            }
+		    PresetMenuEvent::UpdatePresetSelection(name) => {
+                self.selected_preset = name.to_string();
             }
         });
     }
